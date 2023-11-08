701413562ed2f053b02af6814b471536df14e285
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2023-11-08 14:24:59 -0300

    feat: add product page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -25,7 +25,7 @@ export default async function Home() {
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href={`/product/${highlightedProduct.id}`}
+        href={`/product/${highlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 flex items-start justify-center overflow-hidden rounded-lg bg-zinc-900"
       >
         <Image
@@ -53,7 +53,7 @@ export default async function Home() {
       {otherProducts.map((product) => (
         <Link
           key={product.id}
-          href={`/product/${highlightedProduct.id}`}
+          href={`/product/${product.slug}`}
           className="group relative col-span-3 row-span-3 flex items-start justify-center overflow-hidden rounded-lg bg-zinc-900"
         >
           <Image
